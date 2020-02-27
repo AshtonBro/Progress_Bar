f6111596f0564242170451f7cb6d3fc1eac97cc4
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-02-27 22:55:33 +0500

    create ProgressBar func

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 'use strict';
 
 class ProgressBar{
-    constructor(option) {
+    constructor (option = {}) {
         const {
-            startPoint,
-            endPoint,
-            background,
-            height,
-            textColor,
-            border,
+            startPoint = 0,
+            endPoint = 20,
+            background = 'red',
+            height = 20,
+            textColor = 'black',
+            border = '1px solid black',
         } = option;
+
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.background = background;
+        this.height = height;
+        this.textColor = textColor;
+        this.border = border;
+    }
+
+    init(selector) {
+        document.querySelector(selector);
+    }
+
+    createProgressBar(){
+        const progressBar = document.createElement('div');
+    }
+
+    createBar() {
+        const bar = document.createElement('div');
+        bar.style.cssText = `
+            text-align: center;
+            background-color: ${this.background};
+        `;
     }
 }
 
