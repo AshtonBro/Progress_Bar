1abd8b98b0873a80f2f03572931a683becc4cff2
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-02-27 22:28:54 +0500

    const ProgressBar

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+class ProgressBar{
+    constructor(option) {
+        const {
+            startPoint,
+            endPoint,
+            background,
+            height,
+            textColor,
+            border,
+        } = option;
+    }
+}
+
+
